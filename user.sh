b369ca8fae51ba73d8e733bd31a51874aad57be0
vps0 <80207015+vps0@users.noreply.github.com>
2021-04-19 00:20:20 +0000

    Add files via upload

---
+++ user.sh
@@ -0,0 +1,20 @@
+echo "create user, what is the user name?"
+read username
+# create user
+sudo adduser $username
+
+echo "user: " $username "created with success"
+
+echo "add" $username "to SUDO group? (y - yes or n - no)"
+read addRoot
+
+if [ $addRoot == 'y' ]
+then
+    # add user to sudo group
+    sudo groupadd docker
+    sudo usermod -aG docker $username
+    sudo usermod -aG sudo $username
+fi
+
+# list users
+cut -d: -f1 /etc/passwd
